1485206107089000620bdde818b8e44c29c5f10f
Tinaphop   Lapate <el2328@signalschool.ac.th>
2026-06-23 15:36:08 +0700

    Update work05.py

--- w03_Tinaphop/work05.py
+++ w03_Tinaphop/work05.py
@@ -1,1 +1,10 @@
+# ลบข้อมูลใน List
+Fruit = [ "Apple" , "Banana" , "Mango" , "Melon" , "Orange"]
 
+# ลบข้อมูล
+Fruit.Remove("Banana")
+print(Fruit)
+
+Delete_Item = Fruit.Pop(2)
+print(Delete_Item)
+print(Fruit)
