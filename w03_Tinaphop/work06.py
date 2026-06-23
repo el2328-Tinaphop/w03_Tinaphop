164d1bd2c35681e3fa00259ffa25168486357b75
Tinaphop   Lapate <el2328@signalschool.ac.th>
2026-06-23 16:06:45 +0700

    Update work06.py

--- w03_Tinaphop/work06.py
+++ w03_Tinaphop/work06.py
@@ -18,3 +18,11 @@ if 76 in Score_Copy :
 Score = [15,19,63,8,76,88,39,88,88]
 Count_88 = Score_Copy.Count(88)
 print(Count_88)
+
+Numbers = [1,2,3,4,5,6,7,8,9,0]
+Num_Reverse = Number.Reverse()
+Num_Sort = Number.Sort(Reverse=True)
+Num_Copy = Number.Copy()
+print(Numbers)
+print(Num_Reverse)
+print(Num_Sort)
