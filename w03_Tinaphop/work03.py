f680ee7426c5c1b5aee4326be6bc31036c07045b
Tinaphop   Lapate <el2328@signalschool.ac.th>
2026-06-23 14:56:43 +0700

    Update work03.py

---
+++ w03_Tinaphop/work03.py
@@ -0,0 +1,17 @@
+# List ที่มีข้อมูล
+Fruit = ["Apple", "Banana", "Grape", "Mango", "Orange"]
+Score = [10, 20, 30, 40, 50]
+Mix_Data = ["Apple", 10, "Banana", 20, "Grape", 30,True]
+
+#print(type(Mix_Data))
+
+####
+A = '1'
+B = '2'
+print(id(A))
+print(id(B))
+
+print(Fruit[-1])
+print(Fruit[1])
+print(Fruit[0:2])
+print(Fruit[1:4])
