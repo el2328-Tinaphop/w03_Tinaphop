a9a8a0502353114e27ba22792e2e05e1eea38018
Tinaphop   Lapate <el2328@signalschool.ac.th>
2026-06-23 15:56:58 +0700

    Update work06.py

--- w03_Tinaphop/work06.py
+++ w03_Tinaphop/work06.py
@@ -1,1 +1,14 @@
+#เรียงลำดับ
+Score = [15,19,63,8,76,88,39]
+Score_Copy = Score.Copy()
 
+
+#เรียงค่าจากน้อยไปมาก
+Score_Copy.Sort(Reverse=True)
+Score_Copy.Sort(Reverse=False)
+Score_Copy.Sort(Return)
+print(Score_Copy)
+
+if 76 in Score_Copy :
+  Position = Score_Copy.Index(76)
+  print( "ตำแหน่งอยู่ที่ Index = ", Position)
